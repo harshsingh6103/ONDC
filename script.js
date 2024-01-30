0233edf43aaf84e8bdab8f274ced9d7fb3918f74
Harsh Singh <harshsingh127.0.0.1@gmail.com>
2024-01-30 17:21:15 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+ 
+ 
+//  <div id='map' >
+// mapboxgl.accessToken = 'pk.eyJ1IjoiaGFyc2hzaW5naC0wMDEiLCJhIjoiY2xyenpqMTZ2MW11ZzJrdGt3Yjc0OGtiMCJ9.PjaZv19Ckvz5-p2PGLXz0g';
+//     var map = new mapboxgl.Map({
+//         container: 'map',
+//         style: 'mapbox://styles/mapbox/streets-v11'
+//      });
+// </div>
